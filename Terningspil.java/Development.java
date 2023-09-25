5238f2478d7f51fb5b3b720f6c45004085ebadf8
Emil Hansen <s230995@dtu.dk>
2023-09-25 15:03:48 +0200

    Class spiller

--- Terningspil.java/Development.java
+++ Terningspil.java/Development.java
@@ -3,3 +3,21 @@ public class Development {
 }
 import java.util.Random;
 import java.util.Scanner;
+
+class Spiller {
+
+    private int point;
+
+    public Spiller() {
+        this.point = 0;
+    }
+
+    public int getPoint() {
+        return this.point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+
