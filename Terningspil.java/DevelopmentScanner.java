0fac961d0afe51ab89197809bedabbe2e3d802a6
Daud-DTU <144692419+Daud-DTU@users.noreply.github.com>
2023-09-26 14:38:24 +0200

    Update DevelopmentScanner.java

--- Terningspil.java/DevelopmentScanner.java
+++ Terningspil.java/DevelopmentScanner.java
@@ -1,3 +1,23 @@
 public class DevelopmentScanner {
     
 }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter player 1's name:");
+        String player1Name = scanner.nextLine();
+        Player player1 = new Player(player1Name);
+
+        System.out.println("Enter player 2's name:");
+        String player2Name = scanner.nextLine();
+        Player player2 = new Player(player2Name);
+
+        scanner.close();
+
+        DiceGame.playGame(player1, player2);
+    }
+}
+
