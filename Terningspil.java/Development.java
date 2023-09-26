a62757653b11a227fa4900a6f30f46bb80102498
Emil Hansen <s230995@dtu.dk>
2023-09-26 14:22:51 +0200

    tilføjer Development filer

--- Terningspil.java/Development.java
+++ Terningspil.java/Development.java
@@ -1,23 +1,54 @@
-public class Development {
-    
-}
-import java.util.Random;
+
+    import java.util.Random;
 import java.util.Scanner;
+public class Development {
+      private static final int WINNING_SCORE = 40;
+    public static void main(String[] args) {
 
-class Spiller {
+        Scanner scanner = new Scanner(System.in);
 
-    private int point;
+   
+        System.out.println("Indtast spiller 1's navn:");
+        String player1Name = scanner.nextLine();
+        System.out.println("Indtast spiller 2's name:");
+        String player2Name = scanner.nextLine();
 
-    public Spiller() {
-        this.point = 0;
+ 
+        Random random = new Random();
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        while (true) {
+
+
+            System.out.println( player1Name + "'s tur.");
+            int player1Roll1 = random.nextInt(6) + 1;
+            int player1Roll2 = random.nextInt(6) + 1;
+            int player1TurnScore = player1Roll1 + player1Roll2;
+            player1Score += player1TurnScore;
+            System.out.println("Du slog en " + player1Roll1 +" er" +  " og en " + player1Roll2 +" er" +  " i alt " + player1TurnScore + " point.");
+            System.out.println("Din score er " + player1Score + " point.");
+
+            if (player1Score >= WINNING_SCORE) {
+                System.out.println(player1Name + " har vundet!");
+                break;
+            }
+
+            System.out.println( player2Name + "'s tur.");
+            int player2Roll1 = random.nextInt(6) + 1;
+            int player2Roll2 = random.nextInt(6) + 1;
+            int player2TurnScore = player2Roll1 + player2Roll2;
+            player2Score += player2TurnScore;
+            System.out.println("Du slog " + player2Roll1 +" er" +  " og en " + player2Roll2 +" er" +  " i alt " + player2TurnScore + " point.");
+            System.out.println("Din score er " + player2Score + " point.");
+
+            if (player2Score >= WINNING_SCORE) {
+                System.out.println(player2Name + " har vundet!");
+                break;
+            }
+        }
+
+        scanner.close();
     }
-
-    public int getPoint() {
-        return this.point;
-    }
-
-    public void setPoint(int point) {
-        this.point = point;
-    }
-
-
+}
--- Terningspil.java/Main branch.java
+++ Terningspil.java/Main branch.java
@@ -1,3 +1,3 @@
-public class Main branch {
+public class Mainbranch {
     
 }
